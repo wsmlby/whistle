73cf6dd600e0e151857a59ea6ea75bc5f2f69a16
Bingyan Liu <wsmlby@gmail.com>
2025-08-27 00:46:00 -0700

    binutils

--- build.sh
+++ build.sh
@@ -14,6 +14,11 @@ fi
 pip install --upgrade pip
 pip install .[build]
 
+apt list --installed |grep binutils || {
+    echo "binutils is not installed. Installing..."
+    apt update && apt install -y binutils
+}
+
 pyinstaller --onefile --name whistle whistle/__main__.py
 
 echo "Build complete. The binary is in the dist/ directory."
